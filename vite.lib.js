44fb1697683793a217986bb4123b4e0cd39e1efb
Loris Sigrist <sigrist@hasch.io>
2023-04-06 12:02:12 +0200

    Added types to the lib build

--- vite.lib.js
+++ vite.lib.js
@@ -1,21 +1,24 @@
 //The vite config for building the library
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl"
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
     plugins: [glsl({
         compress: true
-    })],
+    }),
+    dts()],
     build: {
         outDir: "lib_dist",
         lib: {
-            entry: "lib/topology.js",
+            entry: "lib/index.js",
             formats: ["es"],
             fileName: "index",
         },
         //I don't like npm packages being minified
         //The user usually has a minifier in their build pipeline 
         //Let them do it
-        minify: false 
+        minify: false,
+        emptyOutDir: true,
     },
 });
